b9bd065f66bdf537fa974d16aaafacc6d7f140f7
mjmartins11 <48074706+mjmartins11@users.noreply.github.com>
2020-07-25 15:55:16 -0300

    Organização do código

--- SpaceHit.asm
+++ SpaceHit.asm
@@ -1,8 +1,11 @@
-; Laboratório de Organização e Arquitetura de Computadores
-; Jogo: Space Hit
-; 
-; Isadora Carolina Siebert   NUSP: 11345580
-; Marlon
+; Laboratorio de Organização e Arquitetura de Computadores
+ 
+; --------------------
+; 	Jogo - SpaceHit
+; --------------------
+  
+; Isadora Carolina Siebert  NUSP: 11345580
+; Marlon José Martins 		NUSP: 10249010
 
 ; ------- TABELA DE CORES -------
 ; adicione ao caracter para Selecionar a cor correspondente
@@ -27,26 +30,25 @@
 ; Coracao provisorio: S2 <3
 jmp main
 
-pos : var #30
-char : var #30
-linha : string "=================================="
-StrPerdeu: string "FIM! Voce perdeu!"
-StrPressEnter: string "Aperte ENTER para"
-StrJogarNovmente: string "jogar novamente!"
-StrOuP: string "Ou 'p' para"
-StrFinalizar: string "finalizar programa" 
+linha:	string "=================================="
+StrPerdeu:	string "FIM! Voce perdeu!"
+StrPressEnter:	string "Aperte ENTER para"
+StrJogarNovmente:	string "jogar novamente!"
+StrOuP:	string "Ou 'p' para"
+StrFinalizar:	string "finalizar programa" 
 
+pos:	var #30
+char:	var #30
+cont:	var #1	; usado para contar o numero de linhas entre cada letra que cai 
+tecla:	var #1 
+score:	var #1
+estado:	var #1
+derrota:	var #1 
+velocidade:	var #1
+pospessoa:	var #1
 
-score : var #1
-pospessoa : var #1
-cont : var #1 ; usado para contar o numero de linhas entre cada letra que cai 
-tecla : var #1 
-derrota : var #1 
-velocidade : var #1
-estado: var #1
-
-; Funcao que reinicia as variaveis do jogo
-reinicia_variaveis_do_jogo:
+; Funcao que inicializa as variaveis do jogo
+inicializa_variaveis_do_jogo:
 	push r0
 	push r1
 	push r2
@@ -67,15 +69,14 @@ reinicia_variaveis_do_jogo:
 	store cont, r0
 	store estado, r0
 	
-	loadn r0 , #5	; posicao pessoa
+	loadn r0, #5	; posicao pessoa
 	store pospessoa, r0
 	
-	loadn r0 , #255
+	loadn r0, #255
 	store tecla, r0
 	
-	loadn r0 , #4000
+	loadn r0, #4000
 	store velocidade, r0
-	
 	
 	pop r2
 	pop r1
@@ -83,22 +84,23 @@ reinicia_variaveis_do_jogo:
 	rts
 
 rand : var #1
-static rand, #53 ; seed inicial
+static rand, #53	; seed inicial
 ; funcao que gera numeros aleatorios de 0 a 255
 ; (SEED * a + c) % p
+
 gera_rand:
 	push r0
 	push r1
 	push r2
 	push r3
 	
-	load r0, rand  ;SEED
-	loadn r1,#97   ;a
-	loadn r2, #133 ;c
-	loadn r3, #256 ;p
-	mul r0,r0,r1   ; SEED*a
-	add r0,r0,r2   ;	SEED*a + c
-	mod r0,r0,r3   ; (SEED*a + c) % p
+	load r0, rand	; SEED
+	loadn r1, #97	; a
+	loadn r2, #133 	; c
+	loadn r3, #256 	; p
+	mul r0, r0, r1   	; SEED*a
+	add r0, r0, r2   	;	SEED*a + c
+	mod r0, r0, r3   	; (SEED*a + c) % p
 	
 	store rand, r0
 	
@@ -111,57 +113,53 @@ gera_rand:
 ; Inicio do Programa Principal
 
 main:
-	call reinicia_variaveis_do_jogo ; ou inicia, apenas zera tudo
+	call inicializa_variaveis_do_jogo ; ou inicia, apenas zera tudo
 	
-	;call Imprimestr   ;  r0 = Posicao da tela que o primeiro caractere da mensagem sera' impresso;  r1 = endereco onde comeca a mensagem; r2 = cor da mensagem.   Obs: a mensagem sera' impressa ate' encontrar "/0"
+	;  r0 = Posicao da tela que o primeiro caractere da mensagem sera' impresso;  r1 = endereco onde comeca a mensagem; r2 = cor da mensagem.   Obs: a mensagem sera' impressa ate' encontrar "/0"
 	call imprimeTelaIni
-	call desenha_nave; mudar para desenha_nave
+	call desenha_nave	; mudar para desenha_nave
 	
 	loadn r3, #1
 	loadn r4, #29
 	loadn r5, #40
 	
 	
-loopmain:	
-
-	;call teste_rand
-	
+loop_main:	
 	call atualizaTela
 	call atualizaScore
 	
 	load r0, velocidade
-	;loadn r1, #119
-	;call imprimeNum
-	
-	call verificaDerrota ; e salva resposta em variavel global derrota
+
+	call verificaDerrota	; e salva resposta em variavel global derrota
 	load r0, derrota
-	cmp r0, r3 ; se derrota == 1
+	cmp r0, r3	; se derrota == 1
 	jeq telaDeDerrota
 	
 	call wait_com_comando
 	
-	
-	jmp loopmain
+	jmp loop_main
 	
 telaDeDerrota:
-	;print DERROTA
-	;PARA RECOMEÇAR CLICAR EM ENTER
-	;CRIAR FUNC Q RESETA VARIAVEIS (???) nao entendi
+	; print DERROTA
+	; PARA RECOMEÇAR CLICAR EM ENTER
+	; CRIAR FUNC Q RESETA VARIAVEIS (???) nao entendi
 	call imprimeTelaDerrota
 	
 	inchar r0
-	loadn r1, #13  ; começa novo jogo se apertar 'Enter'
-	loadn r2, #'p' ; finaliza jogo se apertar 'p'
+	loadn r1, #13  	; começa novo jogo se apertar 'Enter'
+	loadn r2, #'p'	; finaliza jogo se apertar 'p'
 	
 	cmp r0, r2
 	jeq fimPrograma
+	
 	cmp r0, r1
 	jne telaDeDerrota
+	
 	call apagaTelaInteira
 	jmp main
+	
 fimPrograma:	
 	halt
-	
 
 ;---- Fim do Programa Principal -----
 	
@@ -171,10 +169,7 @@ fimPrograma:
 	
 	;print vetor na tela
 	
-	
 ;---- Inicio das Subrotinas -----
-
-
 
 imprimeTelaDerrota:
 	push r0
@@ -206,7 +201,6 @@ imprimeTelaDerrota:
 	loadn r2, #0			; Seleciona a COR da Mensagem
 	call Imprimestr
 	
-	
 	pop r2
 	pop r1
 	pop r0
@@ -220,12 +214,11 @@ apagaTelaInteira:
 apagaTelaInteira_loop:
 	outchar r2, r1
 	inc r1
-	cmp r1,r0
+	cmp r1, r0
 	jne apagaTelaInteira_loop
 	
 	rts
 	
-
 atualizaScore:
 	push r0
 	push r1
@@ -239,24 +232,27 @@ atualizaScore:
 	load r2, pospessoa  ; r2 = pos da pessoa 
 	dec r2
 	
-	;Faz tres comparações pois é o tamanho da bandeja(3 posiçoes)
-	cmp r1,r2 ; r1 == r2-1 ?
+	; Faz tres comparações pois é o tamanho da bandeja(3 posiçoes)
+	cmp r1, r2	; r1 == r2-1 ?
 	jeq AtualizaHit
+	
 	inc r2
-	cmp r1,r2 ; r1 == r2
+	cmp r1, r2 	; r1 == r2
 	jeq AtualizaHit
+	
 	inc r2
-	cmp r1,r2; r1 == r2 +1
+	cmp r1, r2	; r1 == r2 +1
 	jeq AtualizaHit
 
-	;se chegar aqui significa que é um miss, nao atualiza
+	; se chegar aqui significa que é um miss, nao atualiza
 	jmp AtualizaMiss 
+
 AtualizaHit:
-	;outchar x se der hit??
+	; outchar x se der hit??
 	loadn r1, #40
-	storei r0, r1 ; marca como invalido
+	storei r0, r1 	; marca como invalido
 	
-	;call incScore
+	; call incScore
 	load r0, score
 	inc r0
 	store score, r0
@@ -269,8 +265,7 @@ AtualizaMiss:
 	pop r0
 	rts
 
-
-verificaDerrota :
+verificaDerrota:
 	push r0
 	push r1
 	push r2
@@ -281,7 +276,7 @@ verificaDerrota :
 	
 	loadi r1, r0
 	loadn r2, #40
-	cmp r1,r2
+	cmp r1, r2
 	jeq fimVerificaDerrota
 	
 	loadn r1, #1
@@ -300,14 +295,14 @@ executa_comando:
 	load r0, tecla
 	
 	loadn r1, #'a'
-	cmp r0,r1
+	cmp r0, r1
 	jne nao_executa_comando_1
 	
 	call move_esquerda
 	
 nao_executa_comando_1:
 	loadn r1, #'d'
-	cmp r0,r1
+	cmp r0, r1
 	jne nao_executa_comando_2
 	
 	call move_direia
@@ -316,8 +311,6 @@ nao_executa_comando_2:
 	pop r1
 	pop r0
 	rts
-
-
 
 wait_com_comando:
 	push r0
@@ -333,47 +326,48 @@ wait_com_comando:
 
 	loadn r3, #255
 	
-	loadn r1, #100 ; na placa mudar para 10
+	loadn r1, #100 	; na placa mudar para 10
+	
 wait_com_comando1:
-	;loadn r2, #4000
 	load r2, velocidade
 	wait_com_comando2:
 		inchar r4
 
-		cmp r4,r3 ;inchar == 255
+		cmp r4, r3	; inchar == 255
 		jne diff255
-			;call set_flagLidoTeclado0
-			loadn r5,#0
-			jmp wait_com_dps
-		diff255 :
-			;load r5, flagLidoTeclado
-			;loadn r5,#1
-			cmp r5,r0
-			jne wait_com_dps
 			
-			store tecla, r4	
-			;call set_flagLidoTeclado1
-			loadn r5,#1
-			call executa_comando
-		wait_com_dps:
+	loadn r5, #0
+	jmp wait_com_dps
+
+	diff255:
+		cmp r5, r0
+		jne wait_com_dps
 		
+	store tecla, r4	
+	loadn r5, #1
+	call executa_comando
+	
+	wait_com_dps:
 		dec r2
-		cmp r2,r0
+		cmp r2, r0
 		jne wait_com_comando2
+			
 	dec r1
-	cmp r1,r0
+	cmp r1, r0
 	jne wait_com_comando1
 	
-	;VERIFICAR SE VALORES FAZEM SENTIDO AO PASSAR PRA PLACA
+	; VERIFICAR SE VALORES FAZEM SENTIDO AO PASSAR PRA PLACA
 	load r2, velocidade
-	loadn r1, #2010; velocidade maxima vai ser 1990
-	cmp r2,r1
+	loadn r1, #2010	; velocidade maxima vai ser 1990
+	cmp r2, r1
 	jle fim_wait_com_comando
-	loadn r1, #10; step do aumento
-	sub r2,r2,r1
-	store velocidade,r2
+	
+	loadn r1, #10	; step do aumento
+	sub r2, r2, r1
+	store velocidade, r2
+	
 fim_wait_com_comando:
-	store estado,r5
+	store estado, r5
 	pop r5
 	pop r4
 	pop r3
@@ -382,9 +376,6 @@ fim_wait_com_comando:
 	pop r0
 	rts
 
-
-
-
 move_direia:
 	push r0
 	push r1
@@ -392,10 +383,10 @@ move_direia:
 	load r0, pospessoa
 	
 	loadn r1, #34
-	;if(r0 == 38) nao move direita
-	;else move
+	; if(r0 == 38) nao move direita
+	; else move
 	
-	cmp r0,r1
+	cmp r0, r1
 	jeq nao_move_direita
 	
 	inc r0
@@ -403,7 +394,7 @@ move_direia:
 	store pospessoa, r0
 	call desenha_nave
 	
-	call atualizaScore ; permitir pegar a fruta enquanto anda pra cima dela
+	call atualizaScore	; permitir pegar a fruta enquanto anda pra cima dela
 	
 nao_move_direita:
 	pop r1
@@ -428,7 +419,7 @@ move_esquerda:
 	store pospessoa, r0
 	call desenha_nave
 	
-	call atualizaScore ; permitir pegar a fruta enquanto anda pra cima dela
+	call atualizaScore	; permitir pegar a fruta enquanto anda pra cima dela
 	
 nao_move_esquerda:
 	pop r1
@@ -443,7 +434,6 @@ desenha_nave:
 	push r0
 	push r1
 	push r2
-	;loadn r0, #1 ; pos pra desenhar (de 1 a 38)
 	load r0, pospessoa
 	
 ; ----- posicao da primeira linha da nave( /\ )
@@ -529,7 +519,6 @@ desenha_nave:
 	nop
 	outchar r2, r1 
 
-	
 	pop r2
 	pop r1
 	pop r0
@@ -636,12 +625,12 @@ wait:
 	loadn r0, #0
 	
 	loadn r1, #1000
+	
 wait1:
 	loadn r2, #1000
 	wait2:
-		;wait3:
 		dec r2
-		cmp r2,r0
+		cmp r2, r0
 		jne wait2
 	dec r1
 	cmp r1,r0
@@ -667,16 +656,14 @@ atualizaTela:
 	call deleta_nave
 
 	call moveLetras
-
 	
 	loadn r1, #1
 	load r6, cont
-	add r7,r6,r1
+	add r7, r6, r1
 	
 	loadn r1, #8    ; numero de linhas entre cada letra
 	mod r7, r7, r1
 	store cont, r7
-	
 	
 	mov r2, r6
 	
@@ -695,18 +682,18 @@ atualizaTela:
 	load r0, rand
 	
 	loadn r1, #26
-	mod r0,r0,r1 ; gera valor entre 0 e 25(cada um representa um caractere do alfabeto)
+	mod r0, r0, r1 ; gera valor entre 0 e 25(cada um representa um caractere do alfabeto)
 	
 	loadn r1, #'a'
-	add r0,r1,r0 ;transforma de [0 à 25] para [a à z]
+	add r0, r1, r0 ; transforma de [0 à 25] para [a à z]
 
 	call gera_rand
 	load r1, rand
 	
 	loadn r2, #30 ; largura da tela onde as letras surgem
-	mod r1,r1,r2 ; r1 = r1 % 20
+	mod r1, r1, r2 ; r1 = r1 % 20
 	loadn r2, #4
-	add r1,r1,r2
+	add r1, r1, r2
 	
 	storei r6, r1
 	storei r7, r0
@@ -716,7 +703,6 @@ pula_escrita_atualizaTela:
 	call vetNaTela
 	call desenha_nave
 	
-	   
 	pop r7
 	pop r6
 	pop r5
@@ -742,28 +728,27 @@ apagaVetDaTela:
 	loadn r6, #pos
 	loadn r7, #char
 	
-	loadn r0,#30
-	loadn r1,#0 ; i=0
-	
+	loadn r0, #30
+	loadn r1, #0 ; i=0
 
-
-loopApagaVetDaTela:
+loop_ApagaVetDaTela:
 	loadn r2, #40
-	mul r3, r1, r2 ;r3 = linha atual
-	loadi r4, r6 ;r4 = pos na linha
-	loadn r5, #' ' ; r5 = char da linha
+	mul r3, r1, r2 	; r3 = linha atual
+	loadi r4, r6 	; r4 = pos na linha
+	loadn r5, #' ' 	; r5 = char da linha
 	
-	;if(r4 == 40)
-	cmp r4,r2
+	; if(r4 == 40)
+	cmp r4, r2
 	jeq naoPrintEspacoApaga
 	
 	add r4,r4,r3 ; r4 = pos na tela pra imprimir
 	outchar r5, r4
 	
 	loadn r2, #29 ; marcador da linha do '='
-	;if(r4 == 29) 
-	cmp r1,r2
+	; if(r4 == 29) 
+	cmp r1, r2
 	jne naoPrintEspacoApaga
+	
 	loadn r5, #'=';
 	outchar r5, r4
 	
@@ -772,8 +757,7 @@ naoPrintEspacoApaga:
 	inc r7
 	inc r1
 	cmp r1,r0
-	jne loopApagaVetDaTela
-
+	jne loop_ApagaVetDaTela
 	
 	pop r7
 	pop r6
@@ -802,20 +786,21 @@ vetNaTela:
 	loadn r6, #pos
 	loadn r7, #char
 	
-	loadn r0,#30
-	loadn r1,#0 ; i=0
+	loadn r0, #30
+	loadn r1, #0 ; i=0
 	
 	loadn r2, #40 ; marcador do tam da linha
-loopVetNaTela:
+	
+loop_VetNaTela:
 	mul r3, r1, r2 ;r3 = linha atual
 	loadi r4, r6 ; r4 = pos na linha
 	loadi r5, r7 ; r5 = char da linha
 	
 	;if(r4 == 40) goto naoPrintVetNaTela -- significa que nao foi inicializado ainda
-	cmp r4,r2
+	cmp r4, r2
 	jeq naoPrintVetNaTela
 	
-	add r4,r4,r3 ; r4 = pos na tela pra imprimir
+	add r4, r4, r3 ; r4 = pos na tela pra imprimir
 	outchar r5, r4
 	
 naoPrintVetNaTela:
@@ -824,9 +809,7 @@ naoPrintVetNaTela:
 	inc r7	
 	inc r1
 	cmp r1,r0
-	jne loopVetNaTela
-	
-	
+	jne loop_VetNaTela
 	
 	pop r7
 	pop r6
@@ -837,7 +820,6 @@ naoPrintVetNaTela:
 	pop r1
 	pop r0
 	rts
-	
 	
 ;----------------------------------------------------------------------------------------------------------
 ;r0 - end inicial do vet de tam 30
@@ -862,7 +844,8 @@ moveLetras:
 	add r7,r7, r0; r7[final]
 	
 	loadn r0,#29
-loopMoveLetras:
+	
+loop_MoveLetras:
 	loadi r4, r6 ; r4 = pos na linha
 	loadi r5, r7 ; r5 = char da linha
 	
@@ -877,12 +860,9 @@ loopMoveLetras:
 	dec r7
 	dec r7
 	
-	
-	
 	inc r1
 	cmp r1,r0
-	jne loopMoveLetras
-	
+	jne loop_MoveLetras
 	
 	pop r7
 	pop r6
@@ -916,8 +896,9 @@ imprimeTelaIni:
 	loadn r0, #40
 	loadn r1, #0
 	loadn r2, #40
-LoopImprimeTelaIni:
-	mul r3,r2,r1
+	
+loop_ImprimeTelaIni:
+	mul r3, r2, r1
 	loadn r4, #3
 	add r3, r3, r4
 	outchar r5, r3
@@ -926,8 +907,8 @@ LoopImprimeTelaIni:
 	outchar r5, r3
 	
 	inc r1
-	cmp r1,r0
-	jne LoopImprimeTelaIni
+	cmp r1, r0
+	jne loop_ImprimeTelaIni
 	
 	pop r5
 	pop r4
@@ -936,7 +917,6 @@ LoopImprimeTelaIni:
 	pop r1
 	pop r0
 	rts
-
 
 Imprimestr:	;  Rotina de Impresao de Mensagens:    r0 = Posicao da tela que o primeiro caractere da mensagem sera' impresso;  r1 = endereco onde comeca a mensagem; r2 = cor da mensagem.   Obs: a mensagem sera' impressa ate' encontrar "/0"
 	push r0	; protege o r0 na pilha para preservar seu valor
@@ -947,15 +927,16 @@ Imprimestr:	;  Rotina de Impresao de Mensagens:    r0 = Posicao da tela que o pr
 	
 	loadn r3, #'\0'	; Criterio de parada
 
-ImprimestrLoop:	
+loop_Imprimestr:	
 	loadi r4, r1
 	cmp r4, r3
 	jeq ImprimestrSai
+	
 	add r4, r2, r4
 	outchar r4, r0
 	inc r0
 	inc r1
-	jmp ImprimestrLoop
+	jmp loop_Imprimestr
 	
 ImprimestrSai:	
 	pop r4	; Resgata os valores dos registradores utilizados na Subrotina da Pilha
@@ -966,47 +947,48 @@ ImprimestrSai:
 	rts
 	
 ;--------------------------------------------------------------------------------------------------------
-;nao trata tamanho <=0, da ruim -- NAO PASSAR COM TAM <=0 SEUS LOUCOS, NEM FAZ SENTIDO
-;r0 - end do vetor  --- r1 - tam do vetor -- r2 - valor a ser setado
+; nao trata tamanho <= 0, da ruim -- NAO PASSAR COM TAM <= 0 SEUS LOUCOS, NEM FAZ SENTIDO
+; r0 = end do vetor 
+; r1 = tam do vetor
+; r2 = valor a ser setado
 setVetor:
 	loadn r3, #0
-	storei r0, r2
-	inc r0
-	dec r1
-	cmp r1,r3
-	jne setVetor 
+	loop_setVetor:
+		storei r0, r2
+		inc r0
+		dec r1
+		cmp r1, r3
+		jne loop_setVetor 
 	rts
 
 
-;IMPRIMENUMERO
-;param r0 - num
-;param r1 - pos na tela inicial(vai escrever da direita para a esquerda)
+; IMPRIMENUMERO
+; r0 = num
+; r1 = pos na tela inicial(vai escrever da direita para a esquerda)
 imprimeNum:
 	push r0
 	push r1
 	push r3
 	push r4
 	push r5
-	;FAZER PUSHS
 	
-	loadn r5,#'0'
+	loadn r5, #'0'
 	
-loopImprimeNum:
-	loadn r4,#10
+loop_ImprimeNum:
+	loadn r4, #10
 	
-	mod r3,r0,r4; r3 = num % 10
-	add r3,r3,r5; r3 = r3 + '0';
+	mod r3, r0, r4; r3 = num % 10
+	add r3, r3, r5; r3 = r3 + '0';
 	
 	outchar r3, r1
 	
 	dec r1
-	div r0,r0,r4
+	div r0, r0, r4
 	
-	loadn r4,#0
-	cmp r0,r4
-	jne loopImprimeNum
-	
-	;FAZER POPS
+	loadn r4, #0
+	cmp r0, r4
+	jne loop_ImprimeNum
+
 	pop r5
 	pop r4
 	pop r3
@@ -1014,31 +996,29 @@ loopImprimeNum:
 	pop r0
 	rts
 	
-	
-	
 ;Função para testar rodar os numeros rand
 teste_rand:
 	loadn r4, #30
 	loadn r5, #40
 	loadn r6, #4
 	
-	loadn r2, #4 ; i=3
-loopTestRandOut:
+	loadn r2, #4 	; i = 3
+	
+loop_TestRandOut:
 
-	loadn r3, #0 ; j=0
-	loopTestRandIn:
+	loadn r3, #0	; j = 0
+	loop_TestRandIn:
 		load r0, rand
 		call gera_rand
-		mul r1,r3,r5
-		add r1,r1,r2
+		mul r1, r3, r5
+		add r1, r1, r2
 		call imprimeNum
-		inc r3 ;j++
+		inc r3	; j++
 		cmp r3, r4
-		jne loopTestRandIn
+		jne loop_TestRandIn
 		
-	add r2,r2,r6 ;i += 3
+	add r2, r2, r6	; i += 3
 	cmp r2, r5
-	jle loopTestRandOut ;if(i<40)
-	
+	jle loop_TestRandOut	; if(i<40)
 
 	rts
